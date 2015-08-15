e04c54b508f0ba1c87eca4919e7730e1f5747416
Iburinoc <iburinoc@gmail.com>
2015-08-15 17:12:32 -0400

    fixed image handler stuff

--- app/src/main/java/radiancetops/com/resistora/ImageHandler.java
+++ app/src/main/java/radiancetops/com/resistora/ImageHandler.java
@@ -21,13 +21,14 @@ public class ImageHandler implements Camera.PreviewCallback {
     public ImageHandler(int width, int height, int stripheight) {
         super();
 
-        this.width = width;
-        this.height = height;
+        this.width = height;
+        this.height = width;
         this.stripheight = stripheight;
 
         this.H = new double[width * stripheight];
         this.S = new double[width * stripheight];
         this.L = new double[width * stripheight];
+        this.rgb = new int[width * stripheight];
 
         this.Ha = new double[width];
         this.Sa = new double[width];
@@ -64,9 +65,17 @@ public class ImageHandler implements Camera.PreviewCallback {
     }
 
 	public void writeCSV () {
+        double[] h = new double[width], s = new double[width], l = new double[width];
+        int[] rgb = new int[width];
+        for(int i = 0; i < width; i++) {
+            h[i] = Ha[i]; s[i] = Sa[i]; l[i] = La[i];
+            rgb[i] = this.rgb[i];
+        }
         String csv = "";
+        Log.v("data", "w: " + width);
         for (int i = 0; i < width; i++) {
-            csv+=(Ha[i] + ","+ Sa[i]+ "," + La[i]+"\n");
+            //Log.v("data", h[i] + ","+ s[i]+ "," + l[i]);
+            Log.v("rgb", (0xff & (rgb[i] >> 16)) + "," + (0xff & (rgb[i] >> 8)) + "," + (0xff & (rgb[i])));
         }
         /*
 		try {
@@ -77,34 +86,34 @@ public class ImageHandler implements Camera.PreviewCallback {
 			pw.close();
 		} catch (IOException e) {}
 		*/
-        Log.v("data",csv);
-	}
+    }
 
     private void decodeNV21(byte[] data, int width, int height) {
         final int frameSize = width * height;
 
         int a = 0;
-        for (int i = height / 2 - stripheight / 2; i < height / 2 + stripheight / 2; ++i) {
-            for (int j = 0; j < width; ++j) {
-                int y = (0xff & ((int) data[i * width + j]));
-                int v = (0xff & ((int) data[frameSize + (i >> 1) * width + (j & ~1) + 0]));
-                int u = (0xff & ((int) data[frameSize + (i >> 1) * width + (j & ~1) + 1]));
+
+        for (int j = 0; j < width; ++j) {
+            for (int i = height / 2 - stripheight / 2; i < height / 2 + stripheight / 2; ++i) {
+                int y = (0xff & ((int) data[j * height + j]));
+                int v = (0xff & ((int) data[frameSize + (j >> 1) * width + (i & ~1) + 0]));
+                int u = (0xff & ((int) data[frameSize + (j >> 1) * width + (i & ~1) + 1]));
 
                 int rgb = this.rgb[a] = YUVtoRGB(y, u, v);
 
-                int r = 0xff & (rgb >> 16);
-                int g = 0xff & (rgb >>  8);
-                int b = 0xff & (rgb >>  0);
+                double r = (0xff & (rgb >> 16)) / 255.;
+                double g = (0xff & (rgb >>  8)) / 255.;
+                double b = (0xff & (rgb >>  0)) / 255.;
 
-                int max = Math.max(r, Math.max(g, b)), min = Math.min(r, Math.min(g, b));
+                double max = Math.max(r, Math.max(g, b)), min = Math.min(r, Math.min(g, b));
 
-                L[a] = ((max + min) / 2) / 255.;
+                L[a] = ((max + min) / 2);
 
                 if(max == min){
                     H[a] = S[a] = 0; // achromatic
                 } else {
-                    int d = max - min;
-                    S[a] = L[a] > 0.5 ? d / (double) (510 - max - min) : d / (double) (max + min);
+                    double d = max - min;
+                    S[a] = L[a] > 0.5 ? d / (double) (2 - max - min) : d / (double) (max + min);
                     if (max == r) {
                         H[a] = (g - b) / (double) d + (g < b ? 6 : 0);
                     } else if (max == g) {
@@ -122,15 +131,9 @@ public class ImageHandler implements Camera.PreviewCallback {
     private int YUVtoRGB(int y, int u, int v) {
         y = y < 16 ? 16 : y;
 
-        int a0 = 1192 * (y - 16);
-        int a1 = 1634 * (v - 128);
-        int a2 = 832 * (v - 128);
-        int a3 = 400 * (u - 128);
-        int a4 = 2066 * (u - 128);
-
-        int r = (a0 + a1) >> 10;
-        int g = (a0 - a2 - a3) >> 10;
-        int b = (a0 + a4) >> 10;
+        int r = (int) (1.164f * (y - 16) + 1.596f * (v - 128));
+        int g = (int) (1.164f * (y - 16) - 0.813f * (v - 128) - 0.391f * (u - 128));
+        int b = (int) (1.164f * (y - 16) + 2.018f * (u - 128));
 
         r = r < 0 ? 0 : (r > 255 ? 255 : r);
         g = g < 0 ? 0 : (g > 255 ? 255 : g);
