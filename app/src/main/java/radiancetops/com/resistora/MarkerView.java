860d3f37a5a46073992d62383f8be144f2bde42c
Neerajen Sritharan <neerajensritharan@Neerajens-MacBook-Pro.local>
2015-08-22 22:55:21 -0400

    added colors

--- app/src/main/java/radiancetops/com/resistora/MarkerView.java
+++ app/src/main/java/radiancetops/com/resistora/MarkerView.java
@@ -21,6 +21,8 @@ public class MarkerView extends View {
     private float height = -1;
     private boolean firstTime = true;
     private ArrayList<Integer> bandLocation;
+    private ArrayList<Integer> colorIndexes;
+    private int [] presetRGB;
 
 
     public MarkerView(Context context, AttributeSet attrs) {
@@ -31,10 +33,29 @@ public class MarkerView extends View {
         width = -1;
 
         bandLocation = new ArrayList<Integer>();
+        colorIndexes = new ArrayList<Integer>();
+
+        presetRGB = new int [12];
+
+        presetRGB[0] = rgbToInt(0,0,0);
+        presetRGB[1] = rgbToInt(102, 51, 50);
+        presetRGB[2] = rgbToInt(255,0,0);
+        presetRGB[3] = rgbToInt(255, 102, 0);
+        presetRGB[4] = rgbToInt(255, 255, 0);
+        presetRGB[5] = rgbToInt(0, 255, 0);
+        presetRGB[6] = rgbToInt(0, 0, 255);
+        presetRGB[7] = rgbToInt(206, 101, 255);
+        presetRGB[8] = rgbToInt(130, 130, 130);
+        presetRGB[9] = rgbToInt(255, 255, 255);
+        presetRGB[10] = rgbToInt(205, 153, 51);
+        presetRGB[11] = rgbToInt(204, 204, 204);
 
     }
 
-
+    private int rgbToInt(int locR, int locG, int locB){
+        int a = 255;
+        return (((a<<8)+locR<<8)+locG<<8)+locB;
+    }
     public void setup() {
         //Only ran once when the view is first created
         if (!firstTime)
@@ -49,10 +70,11 @@ public class MarkerView extends View {
 
     }//initialisation of the gameboard
 
-    public void setBandLocation (int [] bandLocation){
+    public void setBandLocation (int [] bandLocation, int [] colorIndexes){
         this.bandLocation = new ArrayList<Integer>();
         for (int i = 0; i < bandLocation.length; i++){
             this.bandLocation.add(bandLocation[i]);
+            this.colorIndexes.add(colorIndexes[i]);
         }
         Log.v("band size",""+bandLocation.length);
         invalidate();
@@ -63,7 +85,7 @@ public class MarkerView extends View {
         setup();
         paint.setStrokeWidth(4);
         for (int i = 0; i < bandLocation.size(); i++){
-            paint.setColor(Color.BLACK);
+            paint.setColor(presetRGB[colorIndexes.get(i)]);
             canvas.drawLine(bandLocation.get(i),0 , bandLocation.get(i), height, paint);
             Log.v("band location",""+bandLocation.get(i));
         }
